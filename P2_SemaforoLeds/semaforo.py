b1646729de0153e0c35ccada79a279ff0936d6d4
nocbroae <nocbrodisculpaxdxdd@gmail.com>
2026-04-11 11:22:18 -0600

    Create semaforo.py

---
+++ P2_SemaforoLeds/semaforo.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import RPi.GPIO as GPIO
+import time
+import sys
+
+VERDE = 17
+AMARILLO = 27
+ROJO = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(VERDE, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(AMARILLO, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ROJO, GPIO.OUT, initial=GPIO.LOW)
+
+def cuenta_regresiva(segundos, texto):
+    for i in range(segundos, 0, -1):
+        sys.stdout.write(f"\r{texto} - {i} seg   ")
+        sys.stdout.flush()
+        time.sleep(1)
+    print()
+
+def amarillo():
+    GPIO.output(VERDE, GPIO.LOW)
+    GPIO.output(ROJO, GPIO.LOW)
+    GPIO.output(AMARILLO, GPIO.LOW)
+    time.sleep(0.5)
+
+    for i in range(3, 0, -1):
+        GPIO.output(AMARILLO, GPIO.HIGH)
+        sys.stdout.write(f"\rAMARILLO - {i} seg   ")
+        sys.stdout.flush()
+        time.sleep(1)
+
+        GPIO.output(AMARILLO, GPIO.LOW)
+        time.sleep(0.5)
+
+    print()
+
+try:
+    while True:
+
+        GPIO.output(VERDE, GPIO.HIGH)
+        GPIO.output(AMARILLO, GPIO.LOW)
+        GPIO.output(ROJO, GPIO.LOW)
+        cuenta_regresiva(60, "VERDE")
+
+        GPIO.output(VERDE, GPIO.LOW)
+
+        amarillo()
+
+        GPIO.output(ROJO, GPIO.HIGH)
+        GPIO.output(AMARILLO, GPIO.LOW)
+        cuenta_regresiva(45, "ROJO")
+
+except KeyboardInterrupt:
+    print("\nPrograma interrumpido")
+
+finally:
+    GPIO.cleanup()
+    print("GPIO limpiado")

1b496ca163ad991f86fd2fae1117726e05bea8ea
nocbroae <nocbrodisculpaxdxdd@gmail.com>
2026-05-23 19:15:57 -0600

    Create config.py

---
+++ P11_DHT11_Pantalla_OLED_Grafico/config.py
@@ -0,0 +1,22 @@
+import board
+
+
+# Pines
+I2C_SCL = board.SCL
+I2C_SDA = board.SDA
+DHT_PIN = board.D17
+
+# Pantalla OLED
+OLED_WIDTH = 128
+OLED_HEIGHT = 64
+
+# Parámetros
+TEMP_MAX = 50
+HUM_MAX = 100
+HIST_MAX_POINTS = 128
+REFRESH_DELAY = 2
+ANIM_LIMIT = 2
+
+# Iconos
+ICON_TEMP_PATH = "icons/temp.bmp"
+ICON_HUM_PATH = "icons/hum.bmp"
